88d392435f5eec9cfe72c0c2b2085dc03080e698
VernJet <runners620@gmail.com>
2024-09-02 20:08:17 +0800

    monthly summary function added

--- program/app.py
+++ program/app.py
@@ -1,11 +1,32 @@
 import pandas as pd
+from datetime import datetime
+
+
+def get_month_now():
+    # Get the current date and time
+    now = datetime.now()
+    # Convert to month's name
+    month_name = now.strftime('%B')
+
+    return month_name
+
 
 # Read the CSV file into a DataFrame
 df = pd.read_csv('files/Transactions/July.csv')
 
-# Display the DataFrame
 
-print(df)
+def generate_summary():
+    #Group the data frame according to the category
+    categorized_transaction = df.groupby('Category')
 
-amount = df.groupby(['Category'])
-print("this is amount",amount)
+    #initializing a dictionary to store the summary
+    summary = {}
+
+    for category, details in categorized_transaction:
+        #convert the amount data type and sum up for each category
+        summary[category] = details['Amount'].convert_dtypes().sum()
+
+    return summary
+
+print("Summary of ", get_month_now())
+print(generate_summary())
